e5277198152ffcb80068aaccc275189df588a5d1
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2022-03-16 09:33:51 +0900

    Revert "add anaconda IDE? (spyder)"
    This reverts commit 48f7d6a9237a22b763e6610ef62c092475e55fb6.

--- history.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# *** Spyder Python Console History Log ***
-
-## ---(Wed Mar 16 09:11:45 2022)---
-runfile('C:/Users/rkc32/.spyder-py3/temp.py', wdir='C:/Users/rkc32/.spyder-py3')
